e53439a0e3b30d515e403b8b605622f40627f547
Durag Pal Singh <107455742+duragpal@users.noreply.github.com>
2024-08-18 12:52:41 +0530

    added basic html for lift simulation

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function onSubmit() {
+  const floors = document.getElementById("floors").value;
+  const lifts = document.getElementById("lifts").value;
+  console.log(lifts);
+  const element = document.getElementById("simulation");
+  element.innerHTML = "";
+}
